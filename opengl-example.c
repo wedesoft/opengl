f389654e8d07b76fa5fdabec7f83b7a8e652a810
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-05-21 19:03:12 +0100

    Switched example to GLFW

--- opengl-example.c
+++ opengl-example.c
@@ -1,10 +1,9 @@
-// #include <stdio.h>
+#include <stdio.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int width = 320;
 int height = 240;
-GLuint program;
 
 const char *vertexSource = "#version 130\n\
 in mediump vec3 point;\n\
@@ -20,48 +19,85 @@ void main()\n\
   fragColor = vec3(1, 0, 0);\n\
 }";
 
-// GLuint vao;
-// GLuint vbo;
-// GLuint idx;
-//
-// GLfloat vertices[] = {
-//   -0.5f, -0.5f,  0.0f,
-//    0.5f, -0.5f,  0.0f,
-//   -0.5f,  0.5f,  0.0f,
-//    0.5f,  0.5f,  0.0f
-// };
-//
-// unsigned int indices[] = {0, 1, 3, 2};
-//
-// void handleCompileError(const char *step, GLuint context)
-// {
-//   GLint result = GL_FALSE;
-//   glGetShaderiv(context, GL_COMPILE_STATUS, &result);
-//   if (result == GL_FALSE) {
-//     char buffer[1024];
-//     glGetShaderInfoLog(context, 1024, NULL, buffer);
-//     if (buffer[0])
-//       fprintf(stderr, "%s: %s\n", step, buffer);
-//   };
-// }
-//
-// void handleLinkError(const char *step, GLuint context)
-// {
-//   GLint result = GL_FALSE;
-//   glGetProgramiv(context, GL_LINK_STATUS, &result);
-//   if (result == GL_FALSE) {
-//     char buffer[1024];
-//     glGetProgramInfoLog(context, 1024, NULL, buffer);
-//     if (buffer[0])
-//       fprintf(stderr, "%s: %s\n", step, buffer);
-//   };
-// }
+GLuint vao;
+GLuint vbo;
+GLuint idx;
+
+GLfloat vertices[] = {
+  -0.5f, -0.5f,  0.0f,
+   0.5f, -0.5f,  0.0f,
+  -0.5f,  0.5f,  0.0f,
+   0.5f,  0.5f,  0.0f
+};
+
+unsigned int indices[] = {0, 1, 3, 2};
+
+void handleCompileError(const char *step, GLuint context)
+{
+  GLint result = GL_FALSE;
+  glGetShaderiv(context, GL_COMPILE_STATUS, &result);
+  if (result == GL_FALSE) {
+    char buffer[1024];
+    glGetShaderInfoLog(context, 1024, NULL, buffer);
+    if (buffer[0])
+      fprintf(stderr, "%s: %s\n", step, buffer);
+  };
+}
+
+void handleLinkError(const char *step, GLuint context)
+{
+  GLint result = GL_FALSE;
+  glGetProgramiv(context, GL_LINK_STATUS, &result);
+  if (result == GL_FALSE) {
+    char buffer[1024];
+    glGetProgramInfoLog(context, 1024, NULL, buffer);
+    if (buffer[0])
+      fprintf(stderr, "%s: %s\n", step, buffer);
+  };
+}
 
 int main(void)
 {
   glfwInit();
   GLFWwindow *window = glfwCreateWindow(width, height, "OpenGL example", NULL, NULL);
   glfwMakeContextCurrent(window);
+  glewInit();
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexSource, NULL);
+  glCompileShader(vertexShader);
+  handleCompileError("Vertex shader", vertexShader);
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+  glCompileShader(fragmentShader);
+  handleCompileError("Fragment shader", fragmentShader);
+
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragmentShader);
+  glLinkProgram(program);
+  handleLinkError("Shader program", program);
+
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+               GL_STATIC_DRAW);
+  glGenBuffers(1, &idx);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  glVertexAttribPointer(glGetAttribLocation(program, "point"),
+                        3, GL_FLOAT, GL_FALSE,
+                        3 * sizeof(float), (void *)0);
+
+  glUseProgram(program);
+  glEnableVertexAttribArray(0);
+
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -69,43 +105,5 @@ int main(void)
     glfwPollEvents();
   };
   glfwTerminate();
-  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexSource, NULL);
-  glCompileShader(vertexShader);
-  // handleCompileError("Vertex shader", vertexShader);
-
-  // GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  // glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-  // glCompileShader(fragmentShader);
-  // handleCompileError("Fragment shader", fragmentShader);
-
-  // program = glCreateProgram();
-  // glAttachShader(program, vertexShader);
-  // glAttachShader(program, fragmentShader);
-  // glLinkProgram(program);
-  // handleLinkError("Shader program", program);
-
-  // glGenVertexArrays(1, &vao);
-  // glBindVertexArray(vao);
-
-  // glGenBuffers(1, &vbo);
-  // glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
-  //              GL_STATIC_DRAW);
-  // glGenBuffers(1, &idx);
-  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx);
-  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-  //              GL_STATIC_DRAW);
-
-  // glVertexAttribPointer(glGetAttribLocation(program, "point"),
-  //                       3, GL_FLOAT, GL_FALSE,
-  //                       3 * sizeof(float), (void *)0);
-
-  // glUseProgram(program);
-  // glEnableVertexAttribArray(0);
-
-  // glutDisplayFunc(onDisplay);
-  // glutReshapeFunc(onResize);
-  // glutMainLoop();
   return 0;
 }
